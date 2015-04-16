5f182f836a559474b5bcc1d44ab8ad2330b1a64e
Matthew Lineen <matthew@lineen.com>
2015-04-16 11:39:09 -0500

    add new years eve

---
+++ lib/concrete_holidays/new_years_eve.rb
@@ -0,0 +1,5 @@
+class ConcreteHolidays::NewYearsEve
+  def self.date(year) # December 31st
+    Date.new(year,12,31)
+  end
+end
---
+++ spec/concrete_holidays/new_years_eve_spec.rb
@@ -0,0 +1,11 @@
+require 'concrete_holidays'
+require 'concrete_holidays/new_years_eve'
+
+RSpec.describe ConcreteHolidays::NewYearsEve do
+  let(:holiday) { ConcreteHolidays::NewYearsEve }
+  let(:year) { 2015 }
+
+  it "calculates new years eve" do
+    expect(holiday.date(year)).to eq(Date.new(year,12,31))
+  end
+end
