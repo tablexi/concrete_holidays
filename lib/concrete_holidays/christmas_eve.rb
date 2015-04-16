a003c95e052fa86dee308e9a36f46be3fcdb0505
Matthew Lineen <matthew@lineen.com>
2015-04-16 11:28:59 -0500

    add christmas eve

---
+++ lib/concrete_holidays/christmas_eve.rb
@@ -0,0 +1,5 @@
+class ConcreteHolidays::ChristmasEve
+  def self.date(year) # December 24th
+    Date.new(year,12,24)
+  end
+end
---
+++ spec/concrete_holidays/christmas_eve_spec.rb
@@ -0,0 +1,11 @@
+require 'concrete_holidays'
+require 'concrete_holidays/christmas_eve'
+
+RSpec.describe ConcreteHolidays::ChristmasEve do
+  let(:holiday) { ConcreteHolidays::ChristmasEve }
+  let(:year) { 2015 }
+
+  it "calculates christmas eve" do
+    expect(holiday.date(year)).to eq(Date.new(year,12,24))
+  end
+end
